389b079a29241fd16a76519e22dfeedab4bd2d36
Irdatlanka <konczimre6666@gmail.com>
2022-11-19 04:22:09 +0100

    A working test with CoppeliaSim. Starts simulation, displays speed, ping, etc. in console.

--- SKT-hf/CoppeliaSimTest/CoppeliaSimTest.cpp
+++ SKT-hf/CoppeliaSimTest/CoppeliaSimTest.cpp
@@ -1,10 +1,11 @@
-// faszombeleveremtesztproj.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//------- elso szarnyprobalgatasok------------
+// fasz1.ttt-hez!
 
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <chrono>
+#include <windows.h>
 //#include "stdafx.h"
 
 extern "C" {
@@ -18,40 +19,40 @@ int main(int argc, char* argv[]) {
 	const int portNb = 19997;
 
 	int clientID = simxStart((simxChar*)"127.0.0.1", portNb, true, true, 2000, 5);
-	std::cout << "Client ID: " << clientID << std::endl;
+	std::cout << "Remote API client started. Client ID: " << clientID << std::endl;
 
-	int leftMotorHandle, rightMotorHandle;
-	if (simxGetObjectHandle(clientID, "Pioneer_p3dx_rightMotor", &rightMotorHandle, simx_opmode_blocking) != 0) {
-		std::cout << "Could not retreive handle of right motor." << std::endl;
+	int cube1Handle;
+	if (simxGetObjectHandle(clientID, "Cuboid_1", &cube1Handle, simx_opmode_blocking) != 0) {
+		std::cout << "Could not retreive handle of cuboid1." << std::endl;
 		extApi_sleepMs(5000);
 		return 0;
 	}
 
-	if (simxGetObjectHandle(clientID, "Pioneer_p3dx_leftMotor", &leftMotorHandle, simx_opmode_blocking) != 0) {
-		std::cout << "Could not retreive handle of left motor." << std::endl;
-		extApi_sleepMs(5000);
-		return 0;
-	}
+
 
 	if (clientID != -1)
 	{
-		float motorSpeeds[2];
+		simxFloat cube1VelLin[3]; //velocity; x y z component
+		simxFloat cube1VelAng[3]; //angular velocity; alpha beta gamma
+		simxInt pingTime = 0;
+		simxInt myTestVar = 0;
 
-		motorSpeeds[0] = 3.1415f;
-		motorSpeeds[1] = 3.1415f / 2.0f;
+		simxSynchronous(clientID, true); //turn on stepped mode
+		simxStartSimulation(clientID, simx_opmode_blocking);
 
-		std::cout << "Starting donuts." << std::endl;
-		simxSetJointTargetVelocity(clientID, leftMotorHandle, motorSpeeds[0], simx_opmode_oneshot);
-		simxSetJointTargetVelocity(clientID, rightMotorHandle, motorSpeeds[1], simx_opmode_oneshot);
-
-		extApi_sleepMs(5000);
-
-		std::cout << "Stopping donuts." << std::endl;
-
-		simxSetJointTargetVelocity(clientID, leftMotorHandle, 0.0f, simx_opmode_oneshot);
-		simxSetJointTargetVelocity(clientID, rightMotorHandle, 0.0f, simx_opmode_oneshot);
-
-		extApi_sleepMs(2000);
+		
+		for (int i = 0; i < 200; i++)
+		{
+			simxSynchronousTrigger(clientID); //Next step of simulation
+			simxGetPingTime(clientID, &pingTime); //Ensure that the step is over
+			simxGetObjectVelocity(clientID, cube1Handle, &cube1VelLin[0], &cube1VelAng[0], simx_opmode_blocking);
+			simxGetIntegerSignal(clientID, "myTestVar", &myTestVar, simx_opmode_blocking);
+			printf("Velocity: X:%1.2f Y:%1.2f Z:%1.2f  ||| i:%d  Ping:%d ms TestVar:%d \n", cube1VelLin[0], cube1VelLin[1], cube1VelLin[2], i, pingTime, myTestVar);
+			//Sleep(100);
+		}
+		
+		simxStopSimulation(clientID, simx_opmode_blocking);
+		extApi_sleepMs(500);
 		simxFinish(clientID);
 	}
 
