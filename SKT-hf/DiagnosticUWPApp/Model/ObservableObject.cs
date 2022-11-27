35a06c86145a48debde4e0c6c43e4f42febaf1e9
Attila Fliegl <fliegla@gmail.com>
2022-11-27 17:29:04 +0100

    UWP alkalmazás létrehozása, mappaszerkezet kialakítása, INPC-t implementáló osztály létrehozása, ICommand-ot implementáló osztály létrehozása, 2 IValueConvertert implementáló osztály létrehozása

---
+++ SKT-hf/DiagnosticUWPApp/Model/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DiagnosticUWPApp.Model
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Notify([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SKT-hf/DiagnosticUWPApp/View/IsManualControl2BrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace DiagnosticUWPApp.View
+{
+    public class IsManualControl2BrushConverter : IValueConverter
+    {
+        readonly private static SolidColorBrush red = new SolidColorBrush(Colors.Red);
+        readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.LightGray);
+        readonly private static SolidColorBrush[] brushes = new[] { red, gray };
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return brushes[(int)value];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
